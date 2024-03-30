b222702e7bde60d39ab2c3a3b96a42eaa88a785d
Maria <mariya-ivanova@list.ru>
2024-03-30 14:58:26 +0200

    the solution is ready

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int [] GetAnArray(int size) {
+   int [] array = new int[size];
+   for (int i = 0; i < size; i++){
+    Console.Write("Insert a number: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+   }
+   return array;
+}
+
+int [] array = GetAnArray(8);
+
+Console.Write(string.Join(",", array));
+Console.Write(" -> [");
+Console.Write(string.Join(",", array));
+Console.Write("]");
